23c2350181f3e101ec4390114850045eef562f8d
mateusvoltolim <mateus.voltolim@gmail.com>
2023-01-23 22:02:15 -0300

    feature: init pets adoption server project

---
+++ pets-adoption-server/src/main/java/br/ufg/petsadoption/PetsAdoptionServerApplication.java
@@ -0,0 +1,13 @@
+package br.ufg.petsadoption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsAdoptionServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsAdoptionServerApplication.class, args);
+	}
+
+}
